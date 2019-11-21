e38b719d59db576c29a5e64dafb1ae6107e08856
waitwalker <waitwalker@163.com>
2019-11-21 14:25:03 +0800

    选择身份页面添加

---
+++ lib/pages/login/select_identity_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+///
+/// @name SelectIdentityPage
+/// @description 选择身份页面
+/// @author lca
+/// @date 2019-11-21
+///
+class SelectIdentityPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SelectIdentityState();
+  }
+}
+
+class _SelectIdentityState extends State<SelectIdentityPage> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return null;
+  }
+}
