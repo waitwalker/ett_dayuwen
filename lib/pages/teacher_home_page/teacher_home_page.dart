e30789a1f806ea1604c6e915531c3e5c79132578
waitwalker <waitwalker@163.com>
2019-11-21 13:54:02 +0800

    教师首页页面添加

---
+++ lib/pages/teacher_home_page/teacher_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TeacherHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TeacherHomeState();
+  }
+}
+
+class _TeacherHomeState extends State<TeacherHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
