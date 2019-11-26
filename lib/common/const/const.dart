3fd4b687bf75d20d7a3e25a71a93f473cafa3672
waitwalker <waitwalker@163.com>
2019-11-26 11:47:37 +0800

    接口添加完毕

--- lib/common/const/const.dart
+++ lib/common/const/const.dart
@@ -10,7 +10,32 @@ class Const {
   /// 接口配置
   static const String interfaceConfig = "config.do";
 
+  /// 获取验证码
+  static const String codeInterface = "/sendCode";
+
   /// 登录
-  static const String loginInterface = "login.do";
+  static const String loginInterface = "/login";
+
+  /// 完善用户信息
+  static const String completeUserInfoInterface = "/completeData";
+
+  /// 获取用户信息
+  static const String userInfoInterface = "/user";
+
+  /// 获取七牛上传文件token
+  static const String qiNiuTokenInterface = "/basic/publicUploadToken";
+
+  /// 统计事件
+  static const String appEventInterface = "/appEvent";
+
+  /// 支付验证
+  static const String payVerifyInterface = "/iosPayVerify";
+
+  ///
+  static const String recoveryShoppingInterface = "/recoveryShopping";
+
+  /// 退出登录
+  static const String logoutInterface = "/logOut";
+
 
 }
