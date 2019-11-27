5dcb313796b46ddd2769f6349e2e7a04625ceab3
waitwalker <waitwalker@163.com>
2019-11-27 09:45:34 +0800

    完善信息页面输入框添加

--- lib/pages/login/supplementary_info_page.dart
+++ lib/pages/login/supplementary_info_page.dart
@@ -18,7 +18,9 @@ class SupplementaryInfoPage extends StatefulWidget {
 }
 
 class _SupplementaryInfoState extends State<SupplementaryInfoPage> {
-  WebViewController webViewController;
+  TextEditingController _nameController;
+  FocusNode _nameFocus = FocusNode();
+
 
   @override
   Widget build(BuildContext context) {
@@ -42,13 +44,65 @@ class _SupplementaryInfoState extends State<SupplementaryInfoPage> {
               height: 100,
               width: 100,
               child: FadeInImage.assetNetwork(
-                  placeholder: 'lib/resources/images/student_avatar.png',
-                  image: 'https://pic1.zhimg.com/v2-7fab628481a26f025188b095b5cfafbc.jpg',
-                  width: 100,
-                  height: 100
+                placeholder: 'lib/resources/images/student_avatar.png',
+                image: 'https://pic1.zhimg.com/v2-7fab628481a26f025188b095b5cfafbc.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
               ),
             ),
 
+            Padding(padding: EdgeInsets.only(top: 40)),
+
+            Padding(
+              padding: EdgeInsets.only(top: 40,left: 20,right: 20),
+              child: TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person_outline,color: Colors.grey,size: 24,),
+                  hintText: "填写学生姓名",
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+                onEditingComplete: (){
+
+                },
+
+                onChanged:(text){
+                  print("手机号输入:$text");
+                  if (text.length > 11) {
+                    _nameController.text = text.substring(0,11);
+                  }
+                },
+
+                keyboardType: TextInputType.phone,
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(top: 40,left: 20,right: 20),
+              child: TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person_outline,color: Colors.grey,size: 24,),
+                  hintText: "填写学生年级",
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+                onEditingComplete: (){
+
+                },
+
+                onChanged:(text){
+                  print("手机号输入:$text");
+                  if (text.length > 11) {
+                    _nameController.text = text.substring(0,11);
+                  }
+                },
+
+                keyboardType: TextInputType.phone,
+              ),
+            ),
 
           ],
         ),
