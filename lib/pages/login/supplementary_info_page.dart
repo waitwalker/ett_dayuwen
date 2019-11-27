438ac7d6597339418aace4cc11708ba43ec175be
waitwalker <waitwalker@163.com>
2019-11-27 09:19:54 +0800

    完善资料标题添加

--- lib/pages/login/supplementary_info_page.dart
+++ lib/pages/login/supplementary_info_page.dart
@@ -23,46 +23,15 @@ class _SupplementaryInfoState extends State<SupplementaryInfoPage> {
   Widget build(BuildContext context) {
     return StoreBuilder<AppState>(builder: (context, store) {
       return Scaffold(
-        appBar: AppBar(
-          title: Text("用户协议"),
-          leading: GestureDetector(
-            child: Icon(Icons.arrow_back_ios),
-            onTap: (){
-              Navigator.of(context).pop();
-            },
-          ),
-        ),
-        body: WebView(
-          initialUrl: "https://m.yuwenclub.com/terms.html",
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (controller) {
-            webViewController = controller;
-          },
-          onPageFinished: (url){
-            print("加载完成:$url");
-          },
-          navigationDelegate: (NavigationRequest request) {
-            //对于需要拦截的操作 做判断
-            if(request.url.startsWith("myapp://")) {
-              print("即将打开 ${request.url}");
-              //做拦截处理
-              //pushto....
-              return NavigationDecision.prevent;
-            }
+        body: Column(
+          children: <Widget>[
 
-            //不需要拦截的操作
-            return NavigationDecision.navigate;
-          },
-          javascriptChannels: <JavascriptChannel>[
-            /// js 调用Flutter 只有postMessage或者拦截url
-            JavascriptChannel(
-                name: "message",
-                onMessageReceived: (JavascriptMessage message) {
-                  print("参数： ${message.message}");
-                }
-            ),
-          ].toSet(),
+            Text("完善信息",style: TextStyle(fontSize: 30),),
 
+
+
+
+          ],
         ),
       );
     });
