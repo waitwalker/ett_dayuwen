6385b6db7d6f3eab19ca666f7981cbbea5a9a463
waitwalker <waitwalker@163.com>
2019-11-27 10:03:23 +0800

    收起键盘等事件回调添加

--- lib/pages/login/supplementary_info_page.dart
+++ lib/pages/login/supplementary_info_page.dart
@@ -19,94 +19,130 @@ class SupplementaryInfoPage extends StatefulWidget {
 
 class _SupplementaryInfoState extends State<SupplementaryInfoPage> {
   TextEditingController _nameController;
+  TextEditingController _gradeController;
   FocusNode _nameFocus = FocusNode();
+  FocusNode _gradeFocus = FocusNode();
 
 
   @override
   Widget build(BuildContext context) {
     return StoreBuilder<AppState>(builder: (context, store) {
-      return Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 100)),
-            Container(
-              alignment: Alignment.center,
-              child: Text("完善信息",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-            ),
-            Padding(padding: EdgeInsets.only(top: 40)),
-
-            Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
+      return GestureDetector(
+        onTap: (){
+          _packUpKeyboard();
+        },
+        child: Scaffold(
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 100)),
+              Container(
+                alignment: Alignment.center,
+                child: Text("完善信息",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
               ),
-              height: 100,
-              width: 100,
-              child: FadeInImage.assetNetwork(
-                placeholder: 'lib/resources/images/student_avatar.png',
-                image: 'https://pic1.zhimg.com/v2-7fab628481a26f025188b095b5cfafbc.jpg',
-                width: 100,
+              Padding(padding: EdgeInsets.only(top: 40)),
+
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                ),
                 height: 100,
-                fit: BoxFit.cover,
-              ),
-            ),
-
-            Padding(padding: EdgeInsets.only(top: 40)),
-
-            Padding(
-              padding: EdgeInsets.only(top: 40,left: 20,right: 20),
-              child: TextField(
-                controller: _nameController,
-                focusNode: _nameFocus,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person_outline,color: Colors.grey,size: 24,),
-                  hintText: "填写学生姓名",
-                  hintStyle: TextStyle(color: Colors.grey),
+                width: 100,
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'lib/resources/images/student_avatar.png',
+                  image: 'https://pic1.zhimg.com/v2-7fab628481a26f025188b095b5cfafbc.jpg',
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
                 ),
-                onEditingComplete: (){
-
-                },
-
-                onChanged:(text){
-                  print("手机号输入:$text");
-                  if (text.length > 11) {
-                    _nameController.text = text.substring(0,11);
-                  }
-                },
-
-                keyboardType: TextInputType.phone,
               ),
-            ),
 
-            Padding(
-              padding: EdgeInsets.only(top: 40,left: 20,right: 20),
-              child: TextField(
-                controller: _nameController,
-                focusNode: _nameFocus,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person_outline,color: Colors.grey,size: 24,),
-                  hintText: "填写学生年级",
-                  hintStyle: TextStyle(color: Colors.grey),
+              Padding(padding: EdgeInsets.only(top: 40)),
+
+              Padding(
+                padding: EdgeInsets.only(top: 40,left: 20,right: 20),
+                child: TextField(
+                  controller: _nameController,
+                  focusNode: _nameFocus,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_outline,color: Colors.grey,size: 24,),
+                    hintText: "填写学生姓名",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                  ),
+                  onEditingComplete: (){
+
+                  },
+
+                  onChanged:(text){
+                    print("手机号输入:$text");
+                    if (text.length > 11) {
+                      _nameController.text = text.substring(0,11);
+                    }
+                  },
+
+                  keyboardType: TextInputType.text,
                 ),
-                onEditingComplete: (){
-
-                },
-
-                onChanged:(text){
-                  print("手机号输入:$text");
-                  if (text.length > 11) {
-                    _nameController.text = text.substring(0,11);
-                  }
-                },
-
-                keyboardType: TextInputType.phone,
               ),
-            ),
 
-          ],
+              Padding(
+                padding: EdgeInsets.only(left: 20,right: 20),
+                child: Divider(height: 3.0,color: Colors.grey,),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(top: 40,left: 20,right: 20),
+                child: TextField(
+                  controller: _gradeController,
+                  focusNode: _gradeFocus,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person_outline,color: Colors.grey,size: 24,),
+                    suffixIcon: Icon(Icons.keyboard_arrow_down,color: Colors.grey,size: 30,),
+                    hintText: "填写学生年级",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                  ),
+                  onTap: (){
+                    print("点击这里");
+                    _packUpKeyboard();
+                  },
+                  onEditingComplete: (){
+
+                  },
+
+                  onChanged:(text){
+                    print("手机号输入:$text");
+                    if (text.length > 11) {
+                      _gradeController.text = text.substring(0,11);
+                    }
+                  },
+
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left: 20,right: 20),
+                child: Divider(height: 3.0,color: Colors.grey,),
+              ),
+            ],
+          ),
         ),
       );
     });
+  }
+
+  ///
+  /// @name _packUpKeyboard
+  /// @description 收起键盘
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-11-25
+  ///
+  _packUpKeyboard() {
+    _nameFocus.unfocus();
+    _gradeFocus.unfocus();
   }
 }
